21459a2134df1fde3ece8eebea17bed336516c53
Polya Tomson <polyatomson@gmail.com>
2023-12-06 17:03:51 +0100

    solved day 1

--- 1/app.py
+++ 1/app.py
@@ -1,17 +1,49 @@
 import re
-from timer import my_timer
+from my_timer import my_timer
 
-@my_timer
-def part_one():
-    with open("1/input.txt", "r") as f:
+with open("1/input.txt", "r") as f:
         dat = f.readlines()
 
+@my_timer
+def part_one(dat):
     result = 0
     for line in dat:
         digits = re.findall("[0-9]", line)
         whole_digit = int("".join([digits[0], digits[-1]]))
         result += whole_digit
         
-    print(result)
+    print("result: ", result)
 
-part_one()
+part_one(dat)
+
+def normalize_digits(line):
+    problematic = {"oneight": "oneeight",
+        "twone": "twoone",
+        "threeight": "threeeight",
+        "fiveight": "fiveeight",
+        "sevenine": "sevennine",
+        "eightwo": "eighttwo",
+        "eighthree": "eightthree",
+        "nineight": "nineeight"}
+    for k, v in problematic.items():
+         line = line.replace(k, v)
+    return line
+
+@my_timer
+def part_two(dat):
+    digits_dic = {
+          "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", 
+          "six":"6", "seven":"7", "eight":"8", "nine":"9",
+          "1":"1", "2":"2", "3":"3", "4":"4", "5":"5", "6":"6", "7":"7", "8":"8", "9":"9"
+          }
+    regex = re.compile("one|two|three|four|five|six|seven|eight|nine|[0-9]")
+    result = 0
+    for line in dat:
+        line = normalize_digits(line)
+        digits = re.findall(regex, line)
+        whole_digit = int("".join([digits_dic[digits[0]], digits_dic[digits[-1]]]))
+        result += whole_digit
+    print("result: ", result)
+
+
+part_two(dat)
---
+++ 1/my_timer.py
@@ -0,0 +1,16 @@
+import time
+
+def my_timer(func): #timer wrapper
+
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        print(f"Starting {func_name}")
+        
+        t1 = time.perf_counter()
+        output = func(*args, **kwargs)
+        t2 = time.perf_counter()
+        
+        print(f"Total time for {func_name}: {t2 - t1:.9f} s\n")
+        return output
+    
+    return wrapper
