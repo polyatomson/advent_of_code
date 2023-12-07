a441a0bf1f937d1d417bdeb8f30c46fe0462ab0a
Polya Tomson <polyatomson@gmail.com>
2023-12-07 04:14:24 +0100

    tidied

--- 3/app.py
+++ 3/app.py
@@ -19,11 +19,9 @@ def get_spans(lines):
 def compare_two_spans(span1: tuple[int, int], span2: tuple[int, int]):
     span1 = list(range(span1[0], span1[1]))
     span2 = list(range(span2[0], span2[1]))
-    adjacent = False
     for i in span1:
         if i in span2:
             return True
-    #else
     return False
 
 def main():
@@ -33,13 +31,12 @@ def main():
     
     numbers, characters = get_spans(dat)
     good_numbers = []
+    bad_numbers = []
 
     for i, line in enumerate(numbers):
         if line == []:
             continue
         else:
-            # print(dat[i])
-            
             if i == 0:
                 all_characters_spans = [ char["span"] for line in characters[i:i+2] for char in line]
             elif i == len(dat)-2:
@@ -52,11 +49,9 @@ def main():
                     adjacency = compare_two_spans(number["span"], char_span)
                     if adjacency is True:
                         good_numbers.append(int(number["number"]))
-                        # print(number["number"], i+1)
                         break
                 if adjacency is False:
-                    # print("bad")
-                    print(number["number"], i+1)
+                    bad_numbers.append((number["number"], i+1))
 
     print("result:", sum(good_numbers))
 
