63107d2c47199bc18b6ed3987ed2a2c3e67ebc68
Polya Tomson <polyatomson@gmail.com>
2023-12-07 20:31:17 +0100

    Incorrectly understood the rules

---
+++ 5/app.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from typing import List, Optional
+# import re
+
+@dataclass
+class Mapping:
+    origin_range: range
+    dest_value: int
+
+    def create_mapping(mapping_array: List[int]):
+        return Mapping(origin_range=range(mapping_array[0], mapping_array[1]), 
+                                          dest_value=mapping_array[2])
+
+@dataclass
+class Map:
+    origin: str
+    destination: str
+    mappings: List[Mapping]
+
+    @staticmethod
+    def import_map(raw_map: str):
+        raw_map = raw_map.split("\n")
+        title = raw_map.pop(0).replace(" map:", "")
+        origin, destination = title.split("-to-")
+
+        mappings = [Mapping.create_mapping(
+            [int(n) for n in line.split(" ")]) for line in raw_map]
+        
+        return Map(origin, destination, mappings)
+
+    def get_dest(self, orig_value) -> int:
+        for mapping in self.mappings:
+            if orig_value in mapping.origin_range:
+                return mapping.dest_value
+        else:
+            return orig_value
+
+# @dataclass
+# class SeedInstructions:
+#     seed: int
+#     soil: Optional[int]
+#     fertilizer: Optional[int]
+#     water: Optional[int]
+#     light: Optional[int]
+#     temperature: Optional[int]
+#     humidity: Optional[int]
+#     location: Optional[int]
+
+
+def parse_input(fn: str = "5/input_test.txt") -> (List[int], List[Map]):
+    with open(fn, 'r') as f:
+        dat = f.read()
+    dat = dat.split("\n\n")
+    seeds = dat[0].replace("seeds: ", "")
+    seeds = [int(seed) for seed in seeds.split(" ")]
+    raw_maps = dat[1:]
+    good_maps = [Map.import_map(raw_map) for raw_map in raw_maps]
+    return seeds, good_maps
+
+def go_through_maps(instruction_cursor: int, maps: List[Map]):
+    for map in maps:
+        instruction_cursor = map.get_dest(instruction_cursor)
+    return instruction_cursor
+
+
+def main():
+    seeds, maps = parse_input()
+    for seed in seeds:
+        location = go_through_maps(seed, maps)
+        print(location)
+
+if __name__ == "__main__":
+    main()
