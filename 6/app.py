da27524fe29ab4802f70937f2a6c8fcd2beb73c5
Polya Tomson <polyatomson@gmail.com>
2023-12-08 15:44:45 +0100

    Part Two Solved

--- 6/app.py
+++ 6/app.py
@@ -26,12 +26,26 @@ def load_input(fn: str = "6/input.txt"):
     races = [Race(time=int(n), distance=int(distance_line[i])) for i, n in enumerate(time_line)]
     return races
 
+def load_input_part_two(fn: str = "6/input.txt"):
+    with open(fn, 'r') as f:
+        dat = f.read()
+    dat = dat.replace(" ", "").split("\n")
+    time_line = re.findall("[0-9]+", dat[0])
+    distance_line = re.findall("[0-9]+", dat[1])
+    race = Race(time=int(time_line[0]), distance=int(distance_line[0]))
+    return race
+
 def main():
     races = load_input()
     final_margin = 1
     for race in races:
         final_margin = final_margin * race.button_options()
-    print(final_margin)
+    
+    print("Part One result:", final_margin)
+    
+    race2 = load_input_part_two()
+    final_margin2 = race2.button_options()
+    print("Part Two result:", final_margin2)
 
 if __name__ == "__main__":
     main()
