044eae1cad872ca705b8156b4b2da16deba1a7d5
Polya Tomson <polyatomson@gmail.com>
2023-12-27 15:13:30 +0100

    cycles work

--- 14/part_two.py
+++ 14/part_two.py
@@ -17,44 +17,47 @@ class Board:
         dat = [line.strip('\n') for line in dat]
         return Board(dat)
     
-    def turn_north_from_west(self):
-        cols = [''.join([row[i] for row in self.rows]) for i in range(len(self.rows[0]))]
+    def turn_right(self):
+        # for i in range(len(self.rows[0])):
+        #     for row in reversed(self.rows):
+        #         row[i]
+
+        cols = [''.join([row[i] for row in reversed(self.rows)]) for i in range(len(self.rows[0]))]
         self.rows = cols
-        self.left_headed = 'north'
+        if self.left_headed == 'north':
+            self.left_headed = 'west'
+        elif self.left_headed == 'west':
+            self.left_headed = 'south'
+        elif self.left_headed == 'south':
+            self.left_headed = 'east'
+        elif self.left_headed == 'east':
+            self.left_headed = 'north'
     
-    def turn_west_from_north(self):
-        cols = [''.join([row[i] for row in self.rows]) for i in range(len(self.rows[0]))]
+    def turn_left(self):
+        cols = [''.join([row[i] for row in self.rows]) for i in reversed(range(len(self.rows[0])))]
         self.rows = cols
-        self.left_headed = 'west'
+        if self.left_headed == 'west':
+            self.left_headed = 'north'
+        else:
+            raise Exception('dubious rotation')
     
-    def turn_south_from_west(self):
-        self.turn_north_from_west()
-        self.rows = list(reversed(self.rows))
-        self.left_headed = 'south'
+    # def turn_south_from_west(self):
+    #     self.turn_north_from_west()
+    #     self.rows = list(reversed(self.rows))
+    #     self.left_headed = 'south'
     
-    def turn_east_from_south(self):
-        self.turn_west_from_north()
-        self.left_headed = 'east'
+    # def turn_east_from_south(self):
+    #     self.turn_north_from_west()
+    #     self.rows = list(reversed(self.rows))
+    #     self.left_headed = 'east'
     
-    def turn_north_from_east(self):
-        self.turn_south_from_west()
-        self.left_headed = 'north'
+    # def turn_north_from_east(self):
+    #     self.turn_south_from_west()
+    #     self.left_headed = 'north'
     
     @staticmethod
     def cut_row_into_blocks(row: str):
-        blockings = re.finditer('#+', row)
-        blockings = [match_obj for match_obj in blockings]
-        if blockings == []:
-            return [row]
-        blocks = [row[0:blockings[0].start()]]
-        for x, blocking in enumerate(blockings):
-            if x < len(blockings)-1:
-                start, stop = blocking.start(), blocking.end()
-                blocks.append(row[start:stop])
-                blocks.append(row[stop:blockings[x+1].start()])
-        blocks.append(row[blockings[-1].start():blockings[-1].end()])
-        blocks.append(row[blockings[-1].end():])
-        blocks = [block for block in blocks if block != '']
+        blocks = row.split('#')
         return blocks
 
 
@@ -66,7 +69,7 @@ class Board:
             # spaces = row.split('#')
             # tilted_line = []
             for c, block in enumerate(blocks):
-                if '#' not in block:
+                if '#' not in block and block !='':
                     space_len = len(block)
                     n_rocks = sum([1 for char in block if char == 'O'])
                     n_empty = space_len-n_rocks
@@ -74,9 +77,11 @@ class Board:
                     blocks[c] = tilted_space
 
                 # tilted_line.append(tilted_space)
-            self.rows[i] = ''.join(blocks)
+            self.rows[i] = '#'.join(blocks)
     
     def total_load(self) -> int:
+        if self.left_headed != 'west':
+            raise Exception('turn the west side left to get the northern load')
         res = 0
         for i, line in enumerate(reversed(self.rows)):
             ows = [1 for char in line if char == 'O']
@@ -88,29 +93,53 @@ class Board:
         if self.left_headed != 'north':
             raise Exception("headed the wrong direction")
         self.tilt_left()
-        self.turn_west_from_north()
+        self.turn_right() #west left
+        self.tilt_left() 
+        self.turn_right() #south left
         self.tilt_left()
-        self.turn_south_from_west()
+        self.turn_right() #east left
         self.tilt_left()
-        self.turn_east_from_south()
-        self.tilt_left()
-        self.turn_north_from_east()
+        
+        self.turn_right() # north left again
+    
     
 def main():
     # Part One
-    board = Board.load()
+    board = Board.load('14/input_test.txt')
     print(board)
-    board.turn_north_from_west()
+    board.turn_left() # north left
+    print(board)
     board.tilt_left()
-    board.turn_west_from_north()
+    print(board)
+    board.turn_right() # compare results
     print(board)
     print("Part one result:", board.total_load())
 
     # Part Two
-    board = Board.load()
-    board.turn_north_from_west()
-    for n in range(1000000000):
+    board = Board.load('14/input_test.txt')
+    print(board)
+    board.turn_left() # north left
+    # print(board)
+    # inp = ''
+    n_cycles = 0
+    loads = []
+    # while load != 64:
+    while n_cycles < 200:
+        # inp = input("continue?")
+        n_cycles += 1
         board.cycle()
+        board.turn_right()
+        print(n_cycles, board)
+        load = board.total_load()
+        board.turn_left()
+        # if load in loads:
+        #     print("been_there!")
+        # else:
+        #     print("new")
+        loads.append(load)
+    
+
+
     print("Part two result:", board.total_load())
 
 if __name__ == '__main__':
